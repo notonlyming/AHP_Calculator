846946490ef107d24535c44147884b32016c7ae8
littleming <42286434+notonlyming@users.noreply.github.com>
2018-12-22 16:37:00 +0800

    完成问卷基本界面

--- AHP_Calculator/FormSurvey.Designer.cs
+++ AHP_Calculator/FormSurvey.Designer.cs
@@ -29,27 +29,39 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(410, 382);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(683, 493);
             this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
             // 
             // FormSurvey
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 382);
+            this.ClientSize = new System.Drawing.Size(683, 493);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "FormSurvey";
             this.Text = "FormSurvey";
             this.Load += new System.EventHandler(this.FormSurvey_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- AHP_Calculator/FormSurvey.cs
+++ AHP_Calculator/FormSurvey.cs
@@ -18,6 +18,9 @@ namespace AHP_Calculator
         private string parentFactor;
         private int[] compairIndex;
         private Font defaultFont = new Font(new FontFamily("微软雅黑"), 12, new FontStyle());
+        private ComboBox[] ComboBoxReverseOrNot;
+        private ComboBox[] ComboBoxImportantTimes;
+
         public FormSurvey(string[] factors, string parentFactor)
         {
             InitializeComponent();
@@ -25,6 +28,18 @@ namespace AHP_Calculator
             this.parentFactor = parentFactor;
             labelQuestions = new Label[factors.Length * (factors.Length - 1) / 2];
             pairWiseMatrix = new string[factors.Length, factors.Length];
+            //generate answer control
+            ComboBoxReverseOrNot = new ComboBox[labelQuestions.Length];
+            ComboBoxImportantTimes = new ComboBox[labelQuestions.Length];
+            for (int i = 0; i < ComboBoxImportantTimes.Length; i++)
+            {
+                ComboBoxImportantTimes[i] = new ComboBox();
+                ComboBoxImportantTimes[i].Items.Add("1 - 同等重要");
+                ComboBoxImportantTimes[i].Items.Add("3 - 稍微重要");
+                ComboBoxImportantTimes[i].Items.Add("5 - 较强重要");
+                ComboBoxImportantTimes[i].Items.Add("7 - 强烈重要");
+                ComboBoxImportantTimes[i].Items.Add("9 - 极端重要");
+            }
             //generate what to compare
             compairIndex = new int[labelQuestions.Length * 2];
             int counter = 0;
@@ -43,6 +58,7 @@ namespace AHP_Calculator
         {
             for (int i = 0; i < labelQuestions.Length; i++)
             {
+                //add question
                 labelQuestions[i] = new Label
                 {
                     Text = "Consider two factors: " + factors[compairIndex[2 * i] - 1] + " and " + factors[compairIndex[2 * i + 1] - 1] +
@@ -51,6 +67,12 @@ namespace AHP_Calculator
                     Font = defaultFont
                 };
                 flowLayoutPanel1.Controls.Add(labelQuestions[i]);
+                //add answer control
+                ComboBoxReverseOrNot[i] = new ComboBox();
+                ComboBoxReverseOrNot[i].Items.Add(factors[compairIndex[2 * i] - 1]);
+                ComboBoxReverseOrNot[i].Items.Add(factors[compairIndex[2 * i + 1] - 1]);
+                flowLayoutPanel1.Controls.Add(ComboBoxReverseOrNot[i]);
+                flowLayoutPanel1.Controls.Add(ComboBoxImportantTimes[i]);
             }
         }
     }
