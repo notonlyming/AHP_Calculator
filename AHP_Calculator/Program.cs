776ae534a4562377b0197c7d3b50de0cb15c7490
littleming <42286434+notonlyming@users.noreply.github.com>
2018-12-28 19:17:00 +0800

    完成矩阵运算核心代码部分

--- AHP_Calculator/Program.cs
+++ AHP_Calculator/Program.cs
@@ -24,18 +24,135 @@ namespace AHP_Calculator
 
     public class MatrixOperater
     {
-        public double[,] conventStringMatrixIntoNumberArray()
+        public double[,] conventStringMatrixIntoNumberArray(string[,] stringMatrix)
         {
-            return null;
+            double[,] NumberMatrix = new double[stringMatrix.GetLength(0),stringMatrix.GetLength(1)];
+            for (int i = 0; i < stringMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < stringMatrix.GetLength(0); j++)
+                {
+                    //遍历，并对每个元素进行转换
+                    if (stringMatrix[i,j].Contains('/'))
+                    {
+                        string[] operateNumber = stringMatrix[i,j].Split('/');
+                        NumberMatrix[i,j] = int.Parse(operateNumber[0]) / (double)int.Parse(operateNumber[1]);
+                    }
+                    else
+                    {
+                        NumberMatrix[i,j] = double.Parse(stringMatrix[i,j]);
+                    }
+                }
+            }
+            return NumberMatrix;
         }
 
-        public ArrayList CheckMatrix()
+        public ArrayList CheckMatrix(double[,] PairWiseMatrix)
         {
-            //OK?CI?CR?
-
-            return null;
+            //CI?CR?OK?
+            ArrayList CheckMatrixResult = new ArrayList(3);  //创建容量为3的列表
+            double[] RI = {0,0,0.58,0.90,1.12,1.24,1.32,1.41,1.45};
+            int n = PairWiseMatrix.GetLength(0);
+            double lamadaMax = GetMaxEigenvalueOfMatrix(PairWiseMatrix);
+            double CI = (lamadaMax-n)/(n-1);
+            double CR = CI/RI[n];
+            CheckMatrixResult.Add(CI);
+            CheckMatrixResult.Add(CR);
+            CheckMatrixResult.Add(CR<0.1);
+            return CheckMatrixResult;
         }
 
+        public double GetMaxEigenvalueOfMatrix(double[,] Matrix)
+        {
+            //AWi/Wi的和求算术平均
+            double result = 0;
+            double[] W = GetEigenvactorOfMatrix(Matrix);  //求权向量
+            double[] AW = MatrixMultiplyVactor(Matrix, W);  //求AW乘积
+            //加和
+            for (int i = 0; i < W.Length; i++)
+            {
+                result += AW[i]/W[i];
+            }
+            //求平均
+            result /= Matrix.GetLength(0);
+            return result;
+        }
 
+        public double[] MatrixMultiplyVactor(double[,] Matrix, double[] vactor)
+        {
+            if (Matrix.GetLength(0) == vactor.Length)
+            {
+                double[] resultVactor = new double[vactor.Length];
+                double sum = 0;
+                for (int rowIndex = 0; rowIndex < vactor.Length; rowIndex++)
+                {
+                    sum = 0;
+                    for (int columnIndex = 0; columnIndex < vactor.Length; columnIndex++)
+                    {
+                        //每行元素与向量元素乘积加合
+                        sum += (Matrix[rowIndex,columnIndex]*vactor[columnIndex]);
+                    }
+                    //搞定赋值
+                    resultVactor[rowIndex]  = sum;
+                }
+                return resultVactor;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public double[] GetEigenvactorOfMatrix(double[,] Matrix)
+        {
+            //复制一下
+            double[,] ColumnNormalizedMatrix = new double[Matrix.GetLength(0),Matrix.GetLength(1)];
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(0); j++)
+                {
+                    ColumnNormalizedMatrix[i,j] = Matrix[i,j];
+                }
+            }
+            //将每一列标准化
+            double sum = 0;
+            for (int columnIndex = 0; columnIndex < Matrix.GetLength(0); columnIndex++)
+            {
+                sum = 0;
+                //求该列的和
+                for (int rowIndex = 0; rowIndex < Matrix.GetLength(0); rowIndex++)
+                {
+                    sum += ColumnNormalizedMatrix[rowIndex,columnIndex];
+                }
+                //将该列标准化
+                for (int rowIndex = 0; rowIndex < Matrix.GetLength(0); rowIndex++)
+                {
+                    ColumnNormalizedMatrix[rowIndex,columnIndex] /= sum;
+                }
+            }
+            //求列向量的和
+            double[] w = new double[Matrix.GetLength(0)];
+            for (int rowIndex = 0; rowIndex < w.Length; rowIndex++)
+            {
+                //将这一行的数加起来
+                w[rowIndex] = 0;
+                for (int columnIndex = 0; columnIndex < w.Length; columnIndex++)
+                {
+                    w[rowIndex] += ColumnNormalizedMatrix[rowIndex,columnIndex];
+                }
+            }
+            //w向量归一化
+            //求和
+            sum = 0;
+            for (int i = 0; i < w.Length; i++)
+            {
+                sum += w[i];
+            }
+            //元素除和
+            for (int i = 0; i < w.Length; i++)
+            {
+                w[i] /= sum;
+            }
+            return w;
+        }
     }
 }
